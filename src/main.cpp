57ffcde900ea02cc1ecdf1cd9483a98a3c7fdc58
PosthumousCroissant <138997468+PosthumousCroissant@users.noreply.github.com>
2024-01-04 17:36:30 +0100

    Included namespace in main

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,12 @@
 #include "System.h"     // For initializing and managing SDL
 #include "GameEngine.h" // For the game engine
 
+using namespace cwing;
+
 int main(int argc, char** argv) 
 {
     //cwing::System sys;
-    cwing::GameEngine engine;
+    GameEngine engine;
 
     // ... setup and run the game ...
     engine.run();
